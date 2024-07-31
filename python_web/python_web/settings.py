1710ceb6f5e6a00a81caa45e40c8f0a5bdbedd52
Macbook <macbook@nguyens-MacBook-Pro.local>
2024-07-31 19:41:28 +0700

    second file

--- python_web/python_web/settings.py
+++ python_web/python_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-19!izab%x7j(0m^@(eoqf^a$7y^ph3bk!ocm-9p-*---+-myfm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 
 # Application definition
